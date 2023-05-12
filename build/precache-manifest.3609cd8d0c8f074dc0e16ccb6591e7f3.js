6027329df760ffd43cc54d83ccfe917e96e35ff5
veemasse <veemasse@gmail.com>
2023-05-12 13:46:54 +0300

    fanikauppa

---
+++ build/precache-manifest.3609cd8d0c8f074dc0e16ccb6591e7f3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c3f0ce55f69e7e5dde8",
+    "url": "%3Cgithubtunnus%3E.github.io/fanikauppa/static/css/main.40b7a434.chunk.css"
+  },
+  {
+    "revision": "8c3f0ce55f69e7e5dde8",
+    "url": "%3Cgithubtunnus%3E.github.io/fanikauppa/static/js/main.8c3f0ce5.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "%3Cgithubtunnus%3E.github.io/fanikauppa/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "0c37b377dbbbb7f140c9",
+    "url": "%3Cgithubtunnus%3E.github.io/fanikauppa/static/js/2.0c37b377.chunk.js"
+  },
+  {
+    "revision": "e1055b4b28f5b8816e01",
+    "url": "%3Cgithubtunnus%3E.github.io/fanikauppa/static/js/runtime~main.e1055b4b.js"
+  },
+  {
+    "revision": "667b8ee6e4bc7af048cea10b0cf72f00",
+    "url": "%3Cgithubtunnus%3E.github.io/fanikauppa/static/media/hoodie.667b8ee6.png"
+  },
+  {
+    "revision": "070f1796bc55f14f4715df2f07bd0451",
+    "url": "%3Cgithubtunnus%3E.github.io/fanikauppa/static/media/cap.070f1796.png"
+  },
+  {
+    "revision": "a3fe6343bde728fdc5386627fd206899",
+    "url": "%3Cgithubtunnus%3E.github.io/fanikauppa/static/media/t-shirt.a3fe6343.png"
+  },
+  {
+    "revision": "73efd97151c393b83e15f187db7e7cff",
+    "url": "%3Cgithubtunnus%3E.github.io/fanikauppa/static/media/shoes.73efd971.png"
+  },
+  {
+    "revision": "f99302f0c6aabb607ba263cb15c93d05",
+    "url": "%3Cgithubtunnus%3E.github.io/fanikauppa/index.html"
+  }
+];
